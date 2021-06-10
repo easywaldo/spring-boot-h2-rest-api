3396415eb290eb8259751eb425e8747e64f228c0
easywaldoji <easywaldoji@gmail.com>
2021-06-10 23:40:08 +0900

    테스트 계정 h2 DB 에 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO member (member_seq, member_name, user_id, user_pwd) VALUES (1, '이지남', 'easywaldo', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db');
+INSERT INTO member (member_seq, member_name, user_id, user_pwd) VALUES (2, '홍길동', 'acetious', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db');
